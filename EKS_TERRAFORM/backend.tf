0e50d85849f090a6c9fd6f08ce06693937a74a2f
Ayush Singh <ayush2003sing@gmail.com>
2025-07-26 17:35:56 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ayush-singh986" # Replace with your actual S3 bucket name
+    bucket = "ayush-singh986-eu" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "eu-north-1"
   }
